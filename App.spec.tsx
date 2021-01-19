7ccfa565bf049714793c1de17546d80a941eeb87
Eduardo Gomes <elgsantos@gmail.com>
2021-01-18 22:08:09 -0300

    test: app to render properly

---
+++ App.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
+describe('<App />', () => {
+
+  it('has 1 child', () => {
+    const tree: any = renderer.create(<App />).toJSON();
+    expect(tree?.children?.length).toBe(1);
+  });
+
+  it('matches last snapshot', async () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot();
+  });
+});
