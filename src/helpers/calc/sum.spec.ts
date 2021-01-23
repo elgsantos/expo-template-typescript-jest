f2eeb2ae7344756613dc5cdb083fec16de453298
Eduardo Santos <elgsantos@gmail.com>
2021-01-23 19:48:40 -0300

    test: add faker to get a random number

--- src/helpers/calc/sum.spec.ts
+++ src/helpers/calc/sum.spec.ts
@@ -1,8 +1,10 @@
+import faker from 'faker';
 import sum from './sum'
 
 describe('Sum',()=>{
   it('should return a sum of 2 values', ()=> {
-    const a = 10, b=20
+    const a = faker.random.number(), 
+          b = faker.random.number()
     const result = sum(a,b)
     expect(result).toBe(a+b)
   })
