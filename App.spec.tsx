17d5b9021dec570566bedc521499d0b36ccf2433
Eduardo Santos <elgsantos@gmail.com>
2021-01-23 23:09:40 -0300

    feat: add Image to app

--- App.spec.tsx
+++ App.spec.tsx
@@ -5,9 +5,9 @@ import App from './App';
 
 describe('<App />', () => {
 
-  it('has 1 child', () => {
+  it('has 2 child', () => {
     const tree: any = renderer.create(<App />).toJSON();
-    expect(tree?.children?.length).toBe(1);
+    expect(tree?.children?.length).toBe(2);
   });
 
   it('matches last snapshot', async () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={require('./assets/adaptive-icon.png')} style={styles.logo}/>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,5 +18,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    margin: 10
   },
 });
